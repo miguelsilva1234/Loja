0fc70e280827c1e1a9da253787ed0abef2014b82
miguelsilva1234 <161628496+miguelsilva1234@users.noreply.github.com>
2025-08-22 18:28:55 -0300

    Add files via upload

---
+++ Loja/index.php
@@ -0,0 +1,19 @@
+<?php
+    include 'cabecalho.php';
+?>
+<body>
+    <div class="container">
+        <h1>Bem vindo ao meu primeiro sistema com CRUD</h1>
+        <h2>Vinicius e Miguel</h2>
+        <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+            <a href="form_cadastrar.php" type="button" class="btn btn-danger">Cadastrar</a>
+            <a href="listar.php" type="button" class="btn btn-warning">Listar</a>
+        </div>
+    </div>
+
+
+    <?php
+        include 'rodape.php';
+    ?>
+</body>
+</html>
